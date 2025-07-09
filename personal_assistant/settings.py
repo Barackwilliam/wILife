f25a637b355936cbffdb29cabf288bcfb20c02bb
barackwilliam <barackwilliam12@gmail.com>
2025-07-09 03:24:10 +0300

    added menstruation cycle

--- personal_assistant/settings.py
+++ personal_assistant/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uumrcu3+qqjweuz&l$)yt_n+tsl^colq%wic^m)^m#%zg2*+bs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
